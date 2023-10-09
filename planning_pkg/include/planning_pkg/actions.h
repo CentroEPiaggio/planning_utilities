cdaa032731d52bf503a256ee8296984e2c573631
alessandropalleschi <alessandropalleschi94@gmail.com>
2023-10-09 12:28:26 +0200

    fixed actions definitions for joint and cartesian planning

--- planning_pkg/include/planning_pkg/actions.h
+++ planning_pkg/include/planning_pkg/actions.h
@@ -1,7 +1,6 @@
 #include <ros/ros.h>
 #include <moveit/move_group_interface/move_group_interface.h>
 #include <moveit_msgs/RobotTrajectory.h>
-#include <ros/ros.h>
 #include <actionlib/server/simple_action_server.h>
 #include <planning_msgs/CartesianPlanAction.h>
 #include <planning_msgs/JointPlanAction.h>
@@ -9,23 +8,33 @@
 #include <planning_msgs/ReleaseAction.h>
 #include <Eigen/Dense>
 
-// Define a class template for action servers with feedback, preemption, and goal type
+// Base class for Cartesian Plan Action Server
 class CartesianPlanActionServer
 {
 public:
+    // Constructor for the Cartesian Plan Action Server
     CartesianPlanActionServer(ros::NodeHandle &nh, const std::string &action_name)
         : nh_(nh), action_server_(nh_, action_name, boost::bind(&CartesianPlanActionServer::executeCallback, this, _1), false)
     {
         action_server_.start();
     }
 
-    virtual void executeCallback(const typename planning_msgs::CartesianPlanGoalConstPtr &goal) = 0;
+    // Virtual method to be implemented by derived classes
+    virtual void executeCallback(const planning_msgs::CartesianPlanGoalConstPtr &goal) = 0;
     ~CartesianPlanActionServer();
+
 protected:
     ros::NodeHandle nh_;
     actionlib::SimpleActionServer<planning_msgs::CartesianPlanAction> action_server_;
     bool preempted_;
 
+    // Present joint configuration and computed trajectory
+    std::vector<double> initial_configuration;
+    geometry_msgs::Pose goal_pose;
+    std::string planning_group;
+    trajectory_msgs::JointTrajectory computed_trajectory;
+
+    // Helper function to send feedback to the action client
     void sendFeedback(const planning_msgs::CartesianPlanFeedback &feedback)
     {
         if (!preempted_)
@@ -44,53 +53,93 @@ protected:
         preempted_ = true;
         action_server_.setPreempted();
     }
-    inline bool is_really_null_pose(const geometry_msgs::Pose &pose){
-        if (pose.position.x == 0.0 && pose.position.y == 0.0 && pose.position.z == 0.0 &&
-            pose.orientation.x == 0.0 && pose.orientation.y == 0.0 && pose.orientation.z == 0.0 && pose.orientation.w == 1.0) {
-                return true;
-        }
 
-        return false;
+    // Check if a given pose is really a null pose
+    inline bool isNullPose(const geometry_msgs::Pose &pose)
+    {
+        return (pose.position.x == 0.0 && pose.position.y == 0.0 && pose.position.z == 0.0 &&
+                pose.orientation.x == 0.0 && pose.orientation.y == 0.0 && pose.orientation.z == 0.0 && pose.orientation.w == 1.0);
+    }
+
+    // Check if a given joint trajectory point is really null
+    inline bool isNullPose(const std::vector<double> &joint_configuration)
+    {
+        return joint_configuration.empty();
+        
+        // for (auto &q : joint_configuration)
+        // {
+        //     if (q != 0.0)
+        //     {
+        //         return false; // Found a non-zero value, not null
+        //     }
+        // }
+        // return true; // All values are zero, considered null
     }
 };
 
+// Base class for Joint Plan Action Server
+class JointPlanActionServer
+{
+public:
+    // Constructor for the Joint Plan Action Server
+    JointPlanActionServer(ros::NodeHandle &nh, const std::string &action_name)
+        : nh_(nh), action_server_(nh_, action_name, boost::bind(&JointPlanActionServer::executeCallback, this, _1), false)
+    {
+        action_server_.start();
+    }
 
-// // Define specific action server classes using inheritance
-// class CartesianPlanActionServer : public ActionServerBase<planning_msgs::CartesianPlanAction>
-// {
-// public:
-//     CartesianPlanActionServer(ros::NodeHandle &nh, const std::string &action_name)
-//         : ActionServerBase<planning_msgs::CartesianPlanAction>(nh, action_name)
-//     {
-//     }
+    // Virtual method to be implemented by derived classes
+    virtual void executeCallback(const typename planning_msgs::JointPlanGoalConstPtr &goal) = 0;
+    ~JointPlanActionServer();
 
-//     void executeCallback(const planning_msgs::CartesianPlanGoalConstPtr &goal);
-// private:
-//         inline bool is_really_null_pose(const geometry_msgs::Pose &pose){
-//             if (pose.position.x == 0.0 && pose.position.y == 0.0 && pose.position.z == 0.0 &&
-//                 pose.orientation.x == 0.0 && pose.orientation.y == 0.0 && pose.orientation.z == 0.0 && pose.orientation.w == 1.0) {
-//                     return true;
-//             }
+protected:
+    ros::NodeHandle nh_;
+    actionlib::SimpleActionServer<planning_msgs::JointPlanAction> action_server_;
+    bool preempted_;
 
-//             return false;
-//         }
+    // Present joint configuration and computed trajectory
+    std::vector<double> initial_configuration;
+    std::vector<double> goal_configuration;
+    std::string planning_group;
 
-// };
+    trajectory_msgs::JointTrajectory computed_trajectory;
 
-// class JointPlanActionServer : public ActionServerBase<planning_msgs::JointPlanAction>
-// {
-// public:
-//     JointPlanActionServer(ros::NodeHandle &nh, const std::string &action_name)
-//         : ActionServerBase<planning_msgs::JointPlanAction>(nh, action_name)
-//     {
-//     }
+    // Helper function to send feedback to the action client
+    void sendFeedback(const planning_msgs::JointPlanFeedback &feedback)
+    {
+        if (!preempted_)
+        {
+            action_server_.publishFeedback(feedback);
+        }
+    }
 
-//     void executeCallback(const planning_msgs::JointPlanGoalConstPtr &goal) override
-//     {
-//         // Implement the executeCallback specific to JointPlanAction here
-//     }
-// };
+    bool isPreemptRequested()
+    {
+        return action_server_.isPreemptRequested();
+    }
 
+    void setPreempted()
+    {
+        preempted_ = true;
+        action_server_.setPreempted();
+    }
+    // Check if a given joint trajectory point is really null
+    inline bool isNullPose(const std::vector<double> &joint_configuration)
+    {
+        return joint_configuration.empty();
+        
+        // for (auto &q : joint_configuration)
+        // {
+        //     if (q != 0.0)
+        //     {
+        //         return false; // Found a non-zero value, not null
+        //     }
+        // }
+        // return true; // All values are zero, considered null
+    }
+};
+
+// Uncomment and implement GraspActionServer as needed
 // class GraspActionServer : public ActionServerBase<planning_msgs::GraspAction>
 // {
 // public:
--- planning_pkg/src/actions.cpp
+++ planning_pkg/src/actions.cpp
@@ -1,20 +1,37 @@
-/* Cartesina Plan - Uses moveit movegroupinterface to plan towards a Cartesian pose
-Author: Alessandro Palleschi
-Email: alessandropalleschi94@gmail.com  */
+/**
+ * @file actions.cpp
+ * @brief Implementation of action server classes for Cartesian and Joint planning.
+ *
+ * This file contains the implementations of action server classes for Cartesian and Joint planning
+ * using the MoveIt MoveGroupInterface. It provides functionality to plan trajectories and respond to
+ * action goals.
+ *
+ * @author Alessandro Palleschi
+ * @email alessandropalleschi94@gmail.com
+ */
 
 #include "planning_pkg/actions.h"
 
+/**
+ * @brief Executes the Cartesian planning action server callback.
+ *
+ * This function is the callback for the Cartesian planning action server. It extracts the goal
+ * information, initializes the MoveGroupInterface, sets the goal pose for planning, and plans the
+ * trajectory. The result is set in the action result message.
+ *
+ * @param goal The Cartesian planning action goal.
+ */
 void CartesianPlanActionServer::executeCallback(const planning_msgs::CartesianPlanGoalConstPtr &goal)
 {
-    preempted_ = false;
+    this->preempted_ = false;
 
     // Extract goal information
-    geometry_msgs::Pose goal_pose = goal->goal_pose;
-    geometry_msgs::Pose initial_pose = goal->initial_pose;
-    std::string planning_group = goal->planning_group;
+    this->goal_pose = goal->goal_pose;
+    this->initial_configuration = goal->initial_configuration;
+    this->planning_group = goal->planning_group;
 
     // Check for preemption before starting any computation
-    if (isPreemptRequested())
+    if (this->isPreemptRequested())
     {
         ROS_INFO("CartesianPlanAction: Preempted");
         setPreempted();
@@ -22,33 +39,40 @@ void CartesianPlanActionServer::executeCallback(const planning_msgs::CartesianPl
     }
 
     // Initialize the MoveGroupInterface and joint model group
-    moveit::planning_interface::MoveGroupInterface group(planning_group);
+    moveit::planning_interface::MoveGroupInterface group(this->planning_group);
     const robot_state::JointModelGroup* joint_model_group =
-        group.getCurrentState()->getJointModelGroup(planning_group);
+        group.getCurrentState()->getJointModelGroup(this->planning_group);
 
     // Set the goal pose for planning
-    if (!this->is_really_null_pose(goal_pose))
+    if (!this->isNullPose(this->goal_pose))
     {
-        group.setPoseTarget(goal_pose);
+        group.setPoseTarget(this->goal_pose);
     }
     else
     {
+        ROS_ERROR("Error: you need to specify a valid pose!!");
         return;
     }
 
-    // Create a start state from the initial pose if provided
-    if (!this->is_really_null_pose(initial_pose))
+    // Create a start state from the initial configuration if provided
+    if (!this->isNullPose(this->initial_configuration))
     {
-        robot_state::RobotState start_state(*group.getCurrentState());
-        start_state.setFromIK(joint_model_group, initial_pose);
-        group.setStartState(start_state);
+        moveit::core::RobotStatePtr current_state = group.getCurrentState();
+        current_state->copyJointGroupPositions(joint_model_group, this->initial_configuration);
+        group.setStartState(*current_state);
+    }
+    else
+    {
+        // If initial_pose is null, set the start state to the current state
+        moveit::core::RobotStatePtr current_state = group.getCurrentState();
+        group.setStartState(*current_state); // Use current_state here
     }
 
     // Plan the trajectory
     moveit::planning_interface::MoveGroupInterface::Plan plan;
     moveit::core::MoveItErrorCode planning_result = group.plan(plan);
 
-    if (isPreemptRequested())
+    if (this->isPreemptRequested())
     {
         ROS_INFO("CartesianPlanAction: Preempted");
         setPreempted();
@@ -59,8 +83,87 @@ void CartesianPlanActionServer::executeCallback(const planning_msgs::CartesianPl
     planning_msgs::CartesianPlanResult result;
     if (planning_result == moveit::planning_interface::MoveItErrorCode::SUCCESS)
     {
-        // moveit_msgs::RobotTrajectory trajectory;
-        // plan.trajectory_->getRobotTrajectoryMsg(trajectory);
+        result.planned_trajectory = plan.trajectory_;
+    }
+    else
+    {
+        ROS_ERROR("Planning failed with error code: %d", planning_result.val);
+    }
+
+    // Set the action result
+    action_server_.setSucceeded(result);
+}
+
+/**
+ * @brief Executes the Joint planning action server callback.
+ *
+ * This function is the callback for the Joint planning action server. It extracts the goal
+ * information, initializes the MoveGroupInterface, sets the goal pose for planning, and plans the
+ * trajectory. The result is set in the action result message.
+ *
+ * @param goal The Joint planning action goal.
+ */
+void JointPlanActionServer::executeCallback(const planning_msgs::JointPlanGoalConstPtr &goal)
+{
+    this->preempted_ = false;
+
+    // Extract goal information
+    this->goal_configuration = goal->goal_configuration;
+    this->initial_configuration = goal->initial_configuration;
+    this->planning_group = goal->planning_group;
+
+    // Check for preemption before starting any computation
+    if (this->isPreemptRequested())
+    {
+        ROS_INFO("JointPlanAction: Preempted");
+        this->setPreempted();
+        return;
+    }
+
+    // Initialize the MoveGroupInterface and joint model group
+    moveit::planning_interface::MoveGroupInterface group(this->planning_group);
+    const robot_state::JointModelGroup* joint_model_group =
+        group.getCurrentState()->getJointModelGroup(this->planning_group);
+
+    // Set the goal pose for planning
+    if (!this->isNullPose(this->goal_configuration))
+    {
+        group.setJointValueTarget(this->goal_configuration);
+    }
+    else
+    {
+        return;
+    }
+
+    // Create a start state from the initial configuration if provided
+    if (!this->isNullPose(this->initial_configuration))
+    {
+        moveit::core::RobotStatePtr current_state = group.getCurrentState();
+        current_state->copyJointGroupPositions(joint_model_group, this->initial_configuration);
+        group.setStartState(*current_state);
+    }
+    else
+    {
+        // If initial_pose is null, set the start state to the current state
+        moveit::core::RobotStatePtr current_state = group.getCurrentState();
+        group.setStartState(*current_state); // Use current_state here
+    }
+
+    // Plan the trajectory
+    moveit::planning_interface::MoveGroupInterface::Plan plan;
+    moveit::core::MoveItErrorCode planning_result = group.plan(plan);
+
+    if (this->isPreemptRequested())
+    {
+        ROS_INFO("JointPlanAction: Preempted");
+        this->setPreempted();
+        return;
+    }
+
+    // Create the result message
+    planning_msgs::JointPlanResult result;
+    if (planning_result == moveit::planning_interface::MoveItErrorCode::SUCCESS)
+    {
         result.planned_trajectory = plan.trajectory_;
     }
     else
