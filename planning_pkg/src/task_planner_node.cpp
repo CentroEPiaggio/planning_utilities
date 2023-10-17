5b014a935c87977885ebdd86a181e41e62e6d6ac
Alessandro Palleschi <42964650+alessandropalleschi@users.noreply.github.com>
2023-10-17 17:22:48 +0200

    Update task_planner_node.cpp

--- planning_pkg/src/task_planner_node.cpp
+++ planning_pkg/src/task_planner_node.cpp
@@ -4,15 +4,26 @@
 #include <variant>
 #include <actionlib/client/simple_action_client.h>
 
+// Define a variant type for task parameters
 using TaskParameters = std::variant<planning_msgs::CartesianPlanGoal, planning_msgs::JointPlanGoal>;
 
+/**
+ * @brief Class to handle task execution using ROS action clients.
+ */
 class TaskHandler {
 public:
-    TaskHandler(ros::NodeHandle& nh) : nh_(nh) {
-        // Initialize action clients
-        cartesian_client_.reset(new actionlib::SimpleActionClient<planning_msgs::CartesianPlanAction>("cartesian_plan_action", true));
-        joint_client_.reset(new actionlib::SimpleActionClient<planning_msgs::JointPlanAction>("joint_plan_action", true));
-        execute_client_.reset(new actionlib::SimpleActionClient<planning_msgs::ExecutePlanAction>("execute_plan_action", true));
+    /**
+     * @brief Constructor for TaskHandler.
+     * 
+     * @param nh Reference to the ROS NodeHandle.
+     */
+    TaskHandler(ros::NodeHandle& nh)
+        : nh_(nh),
+          is_executing_(false),
+          // Initialize action clients for Cartesian and Joint planning
+          cartesian_client_(new actionlib::SimpleActionClient<planning_msgs::CartesianPlanAction>("cartesian_plan_action", true)),
+          joint_client_(new actionlib::SimpleActionClient<planning_msgs::JointPlanAction>("joint_plan_action", true)),
+          execute_client_(new actionlib::SimpleActionClient<planning_msgs::ExecutePlanAction>("execute_plan_action", true)) {
 
         // Wait for action servers to start
         if (!cartesian_client_->waitForServer(ros::Duration(5.0)) ||
@@ -23,27 +34,36 @@ public:
         }
     }
 
-    void executePlan(const TaskParameters& goal, bool& is_executing) {
+    /**
+     * @brief Execute a task based on the provided goal.
+     * 
+     * @param goal Task parameters as a variant type.
+     */
+    void executePlan(const TaskParameters& goal) {
         if (std::holds_alternative<planning_msgs::CartesianPlanGoal>(goal)) {
             planning_msgs::CartesianPlanGoal cartesian_goal = std::get<planning_msgs::CartesianPlanGoal>(goal);
+            // Send Cartesian planning goal to the action server
             cartesian_client_->sendGoal(cartesian_goal);
             cartesian_client_->waitForResult();
 
             if (cartesian_client_->getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
                 ROS_INFO("Cartesian plan succeeded.");
-                handleExecution(cartesian_goal.planning_group, *cartesian_client_, is_executing);
+                // Handle execution if it's in progress or set a new goal
+                handleExecution(cartesian_goal.planning_group, *cartesian_client_);
             } else {
                 ROS_ERROR("Cartesian plan failed.");
                 ros::shutdown();
             }
         } else if (std::holds_alternative<planning_msgs::JointPlanGoal>(goal)) {
             planning_msgs::JointPlanGoal joint_goal = std::get<planning_msgs::JointPlanGoal>(goal);
+            // Send Joint planning goal to the action server
             joint_client_->sendGoal(joint_goal);
             joint_client_->waitForResult();
 
             if (joint_client_->getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
                 ROS_INFO("Joint plan succeeded.");
-                handleExecution(joint_goal.planning_group, *joint_client_, is_executing);
+                // Handle execution if it's in progress or set a new goal
+                handleExecution(joint_goal.planning_group, *joint_client_);
             } else {
                 ROS_ERROR("Joint plan failed.");
                 ros::shutdown();
@@ -53,27 +73,36 @@ public:
 
 private:
     ros::NodeHandle& nh_;
+    bool is_executing_;
     std::shared_ptr<actionlib::SimpleActionClient<planning_msgs::CartesianPlanAction>> cartesian_client_;
     std::shared_ptr<actionlib::SimpleActionClient<planning_msgs::JointPlanAction>> joint_client_;
     std::shared_ptr<actionlib::SimpleActionClient<planning_msgs::ExecutePlanAction>> execute_client_;
 
-    void handleExecution(const std::string& planning_group, actionlib::SimpleActionClient<planning_msgs::ExecutePlanAction>& client, bool& is_executing) {
-        if (is_executing) {
+    /**
+     * @brief Handle the execution of a planned task.
+     * 
+     * @param planning_group The name of the planning group.
+     * @param client The action client for execution.
+     */
+    void handleExecution(const std::string& planning_group, actionlib::SimpleActionClient<planning_msgs::ExecutePlanAction>& client) {
+        if (is_executing_) {
             client.waitForResult();
             if (client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
                 planning_msgs::ExecutePlanGoal execute_goal;
                 execute_goal.move_group_name = planning_group;
                 execute_goal.motion_plan = client.getResult()->planned_trajectory;
+                // Send the execution goal to the action server
                 execute_client_->sendGoal(execute_goal);
             } else {
                 ROS_ERROR("Execution FAILED!");
                 ros::shutdown();
             }
         } else {
-            is_executing = true;
+            is_executing_ = true;
             planning_msgs::ExecutePlanGoal execute_goal;
             execute_goal.move_group_name = planning_group;
             execute_goal.motion_plan = client.getResult()->planned_trajectory;
+            // Send the execution goal to the action server
             execute_client_->sendGoal(execute_goal);
         }
     }
@@ -85,8 +114,6 @@ int main(int argc, char** argv) {
 
     TaskHandler task_handler(nh);
 
-    bool is_executing = false;
-
     // Retrieve the task list from the ROS parameter server as XML-RPC data
     XmlRpc::XmlRpcValue task_list;
     if (nh.getParam("/tasks", task_list)) {
@@ -95,7 +122,7 @@ int main(int argc, char** argv) {
             for (int i = 0; i < task_list.size(); ++i) {
                 XmlRpc::XmlRpcValue task = task_list[i];
                 TaskParameters goal = convertYamlToGoal(task);
-                task_handler.executePlan(goal, is_executing);
+                task_handler.executePlan(goal);
             }
         } else {
             ROS_ERROR("Task List is Empty!");
