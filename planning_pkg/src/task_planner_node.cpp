34fdd36a373d984efd5927bd5327557d49054247
Alessandro Palleschi <42964650+alessandropalleschi@users.noreply.github.com>
2023-10-17 17:25:17 +0200

    Update task_planner_node.cpp

--- planning_pkg/src/task_planner_node.cpp
+++ planning_pkg/src/task_planner_node.cpp
@@ -6,6 +6,81 @@
 
 // Define a variant type for task parameters
 using TaskParameters = std::variant<planning_msgs::CartesianPlanGoal, planning_msgs::JointPlanGoal>;
+// Function to parse a Pose from XML-RPC data
+geometry_msgs::Pose parsePose(const XmlRpc::XmlRpcValue& pose_param) {
+    geometry_msgs::Pose pose;
+    pose.position.x = static_cast<double>(pose_param["position"]["x"]);
+    pose.position.y = static_cast<double>(pose_param["position"]["y"]);
+    pose.position.z = static_cast<double>(pose_param["position"]["z"]);
+    pose.orientation.x = static_cast<double>(pose_param["orientation"]["x"]);
+    pose.orientation.y = static_cast<double>(pose_param["orientation"]["y"]);
+    pose.orientation.z = static_cast<double>(pose_param["orientation"]["z"]);
+    pose.orientation.w = static_cast<double>(pose_param["orientation"]["w"]);
+    return pose;
+}
+
+// Function to convert YAML data to goal message
+TaskParameters convertYamlToGoal(const XmlRpc::XmlRpcValue& task_param) {
+    if (task_param.hasMember("type")) {
+        std::string task_type = static_cast<std::string>(task_param["type"]);
+
+        if (task_type == "CartesianPlan") {
+            // Handle Cartesian plan goal creation
+            planning_msgs::CartesianPlanGoal cartesian_goal;
+
+            if (task_param.hasMember("goal")) {
+                cartesian_goal.goal_pose = parsePose(task_param["goal"]);
+            }
+
+            if (task_param.hasMember("initial") && task_param["initial"].getType() == XmlRpc::XmlRpcValue::TypeArray) {
+                for (int i = 0; i < task_param["initial"].size(); ++i) {
+                    if (task_param["initial"][i].getType() == XmlRpc::XmlRpcValue::TypeDouble) {
+                        double value = static_cast<double>(task_param["initial"][i]);
+                        cartesian_goal.initial_configuration.push_back(value);
+                    }
+                }
+            }
+
+            if (task_param.hasMember("group")) {
+                cartesian_goal.planning_group = static_cast<std::string>(task_param["group"]);
+            }
+
+            return cartesian_goal;
+        } else if (task_type == "JointPlan") {
+            // Handle Joint plan goal creation
+            planning_msgs::JointPlanGoal joint_goal;
+
+            if (task_param.hasMember("goal")) {
+                // Extract and assign the goal parameters for JointPlan
+                XmlRpc::XmlRpcValue goal = task_param["goal"];
+                for (int i = 0; i < goal.size(); ++i) {
+                    if (goal[i].getType() == XmlRpc::XmlRpcValue::TypeDouble) {
+                        double value = static_cast<double>(goal[i]);
+                        joint_goal.goal_configuration.push_back(value);
+                    }
+                }
+            }
+
+            if (task_param.hasMember("initial") && task_param["initial"].getType() == XmlRpc::XmlRpcValue::TypeArray) {
+                for (int i = 0; i < task_param["initial"].size(); ++i) {
+                    if (task_param["initial"][i].getType() == XmlRpc::XmlRpcValue::TypeDouble) {
+                        double value = static_cast<double>(task_param["initial"][i]);
+                        joint_goal.initial_configuration.push_back(value);
+                    }
+                }
+            }
+
+            if (task_param.hasMember("group")) {
+                joint_goal.planning_group = static_cast<std::string>(task_param["group"]);
+            }
+
+            return joint_goal;
+        }
+    }
+
+    ROS_ERROR("Wrong task parameters!");
+    return TaskParameters(); // Return an empty variant in case of an error
+}
 
 /**
  * @brief Class to handle task execution using ROS action clients.
